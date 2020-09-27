5b258ab0576fffb26df336e46789863d51ae8284
jethro-magaji <jemagaji38@gmail.com>
2020-09-27 14:34:20 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/waveconnect/wave_connect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.waveconnect.wave_connect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/navigation/messages_screen.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(Icons.ac_unit),
+          Icon(Icons.access_alarms),
+        ],
+      ),
+    );
+  }
+}
